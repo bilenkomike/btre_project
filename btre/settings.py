bc39d57a8a61b07ee8150335a7d5b83bb5f4a9cb
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-10-08 14:31:45 +0300

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -160,13 +160,6 @@ MESSAGE_TAGS = {
 
 
 
-#email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bilenkomike18@gmail.com'
-EMAIL_HOST_PASSWORD = 'yndqwdldrhbuyidr'
-EMAIL_USE_TLS = True
-
 try:
     from .local_settings import *
 except ImportError:
